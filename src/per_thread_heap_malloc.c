51ecb01b1c64381b3ed93b83e71907eb1a6f2776
Ayush Yadav <ayushyadav.ay42@gmail.com>
2025-04-16 22:31:59 -0400

    removed main from per_thread_heap

--- src/per_thread_heap_malloc.c
+++ src/per_thread_heap_malloc.c
@@ -5,6 +5,8 @@
 #include <sys/mman.h>
 #include <unistd.h>
 #include <pthread.h>
+#include<stdlib.h>
+#include<stdlib.h>
 #include <math.h>
 
 void *global_super_block_pointer[64] = {NULL};
@@ -245,48 +247,3 @@ void my_free(void *ptr) {
     pthread_mutex_unlock(&lock[block->my_super_block->owner_thread_id]);
 }
 
-/*
-CFLAGS = -Xpreprocessor -fopenmp \
-         -I/opt/homebrew/opt/libomp/include \
-         -L/opt/homebrew/opt/libomp/lib \
-         -lomp -lm -Wall -Iinclude
-*/
-
-
-
-#define ASSERT(expr) \
-if (!(expr)) { \
-printf("\033[0;31mFAILED: Correctness test failed!\033[0m\n"); \
-exit(1); \
-}
-
-#define NUM_THREADS 8
-#define NUM_ROWS 10000
-#define NUM_COLS 1000
-
-int main() {
-    omp_set_num_threads(NUM_THREADS);
-    int *matrix[NUM_ROWS];
-
-#pragma omp parallel for
-    for (int i = 0; i < NUM_ROWS; i++) {
-        matrix[i] = (int *)my_malloc(sizeof(int)*NUM_COLS);
-        assert(matrix[i] != NULL);
-
-        for (int j = 0; j < NUM_COLS; j++) {
-            matrix[i][j] = i+j;
-        }
-    }
-
-    for(int i=0; i<NUM_ROWS; i++) {
-        for(int j=0; j<NUM_COLS; j++) {
-            ASSERT(matrix[i][j] == i+j);
-        }
-    }
-
-    for (int i=0; i<NUM_ROWS; i++) {
-        my_free(matrix[i]);
-    }
-    printf("\033[0;32mPASS: Correctness test passed!\033[0m\n");
-    return 0;
-}
